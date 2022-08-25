b42ffa83ef8f05a1d677f5e7fd6a56405d8fbde8
Parker Lee <parker.lee.1295@gmail.com>
2022-08-25 09:01:09 -0500

    add gtm

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,6 +45,15 @@ const Excerpt = styled.p`
 const User = props => (
   <div>
     <Helmet>
+      <script>
+        (function(w,d,s,l,i){
+        w[l] = w[l] || [];
+        w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+        var f=d.getElementsByTagName(s)[0], j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+        j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+        f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-MH7ZBLH');
+      </script>
       <script async defer src="https://storage.googleapis.com/lucky-orange-staging-public/core/lo.js?site-id=dd6ae100"></script>
     </Helmet>
     <UserWrapper>
@@ -60,6 +69,8 @@ const User = props => (
 export default function UsersList() {
   return (
     <Container>
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MH7ZBLH"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
       <h1>About Styled Components</h1>
       <p>Styled Components is cool</p>
       <User
